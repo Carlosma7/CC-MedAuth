7b57f07f9c61f853ba0dc8c69ab3c0014150e50d
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-11-02 02:27:42 +0100

    #45 Update main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
-from models.poliza import Poliza
-from models.usuario import Usuario
-from models.autorizacion import Autorizacion
-from models.cita import Cita
-from models.prescripcion import Prescripcion
+from models.poliza.poliza import Poliza
+from models.usuario.usuario import Usuario
+from models.autorizacion.autorizacion import Autorizacion
+from models.cita.cita import Cita
+from models.prescripcion.prescripcion import Prescripcion
 
 if __name__ == '__main__':
     p = Poliza
