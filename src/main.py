ed59f70952172b7e9af9575f186af8529db2c266
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-11-02 01:46:53 +0100

    #45 Update main

--- src/main.py
+++ src/main.py
@@ -11,5 +11,5 @@ if __name__ == '__main__':
     c = Cita
     r = Prescripcion
 
-    print('Todos los elementos se han creado. Sin sintácticamente correctos.')
+    print('Todos los elementos se han creado. Son sintácticamente correctos.')
 
