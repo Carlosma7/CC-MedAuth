6891261a56d55da8844e25e5841116af5be7aa88
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-11-02 02:20:44 +0100

    #37 #38 #39 #40 Creación controlador autorizacion

---
+++ src/models/autorizacion/autorizacionController.py
@@ -0,0 +1,19 @@
+from .autorizacion import Autorizacion
+from typing import List
+
+class AutorizacionController:
+
+	self.autorizaciones: List[Autorizacion]
+	
+	
+	def get_autorizacion(self, id_autorizacion: str):
+		return
+	
+	def set_autorizacion(self, id_autorizacion: str):
+		return
+		
+	def cambiar_estado_autorizacion(self, id_autorizacion: str, estado: str):
+		return
+	
+	def solicitar_autorizacion(self, id_autorizacion: str):
+		return
