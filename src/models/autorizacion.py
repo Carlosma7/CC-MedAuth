14f0befc9afff9a4ca64ff1ae32ade8ca33ba37f
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-11-02 01:37:57 +0100

    #37 #38 Creación modelo autorización

---
+++ src/models/autorizacion.py
@@ -0,0 +1,18 @@
+from .usuario import Usuario
+from typing import List
+import datetime
+
+
+class Poliza:
+
+    def __init__(self):
+        self.id_autorizacion: str
+        self.asegurado: Usuario
+        self.id_prescripcion: str
+        self.id_poliza: str
+        self.estado: str
+        self.motivo_rechazo: str
+        self.fecha_realizacion: str
+        self.servicios_aceptados: List[str]
+        self.facultativo_realizador: str
+        self.consulta: str
