d108338c83264dc8bbe2225d25aea897dcf7dc95
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-11-02 02:09:55 +0100

    #34 #35 Creación controlador poliza

---
+++ src/models/poliza/polizaController.py
@@ -0,0 +1,13 @@
+from .poliza import Poliza
+from typing import List
+
+class PolizaController:
+
+	self.polizas: List[Poliza]
+	
+	
+	def get_poliza(nombre: str):
+		return
+
+	def set_poliza(nombre: str):
+		return
