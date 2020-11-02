ce61f12347fa87e2b06a024b89e3a82746e9ddb4
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-11-02 01:33:32 +0100

    #36 Creación modelo prescripción

---
+++ src/models/prescripcion.py
@@ -0,0 +1,17 @@
+from .usuario import Usuario
+from typing import List
+import datetime
+
+
+class Prescripcion:
+
+    def __init__(self):
+        self.id_prescripcion: str
+        self.asegurado: Usuario
+        self.id_poliza: str
+        self.fecha_realizacion: datetime
+        self.especialidad: str
+        self.facultativo_prescriptor: str
+        self.facultativo_realizador: str
+        self.servicios_solicitados: List[str]
+        self.consulta: str
