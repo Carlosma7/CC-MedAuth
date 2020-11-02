66f0a901411e6d421b857fbb1ce44433e8b8f7d5
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-11-02 01:46:31 +0100

    #45 Update main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
 from models.poliza import Poliza
 from models.usuario import Usuario
+from models.autorizacion import Autorizacion
+from models.cita import Cita
+from models.prescripcion import Prescripcion
 
 if __name__ == '__main__':
     p = Poliza
     u = Usuario
+    a = Autorizacion
+    c = Cita
+    r = Prescripcion
 
-    print('Usuario y poliza creados')
+    print('Todos los elementos se han creado. Sin sintácticamente correctos.')
 
