4a11891e85cbcfe2176bf16098f6bc68c3156122
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-11-07 11:36:30 +0100

    #43 #58 Creación test modelo usuario administrativo

---
+++ src/test/modelos/testUsuarioAdmin.py
@@ -0,0 +1,42 @@
+from .testUsuario import TestUsuario
+import pytest
+
+
+class TestUsuarioAdmin(TestUsuario):
+	def __init__(self, nombre: str, email: str, dni: str, email_empresarial: str):
+		self.__nombre = nombre
+		self.__email = email
+		self.__dni = dni
+		self.__email_empresarial = email_empresarial
+		
+	def get_nombre(self):
+		return self.__nombre
+	
+	def set_nombre(self, nombre):
+		self.__nombre = nombre
+		
+	def get_email(self):
+		return self.__email
+		
+	def set_email(self, email):
+		self.__email = email
+	
+	def get_dni(self):
+		return self.__dni
+		
+	def get_email_empresarial(self):
+		return self.__email_empresarial
+	
+	def set_email_empresarial(self, email_empresarial):
+		self.__email_empresarial = email_empresarial
+	
+	def __eq__(self, otra):
+		return (self.__nombre == otra.get_nombre() ) and (self.__email == otra.get_email()) and (self.__dni == otra.get_dni()) and (self.__email_empresarial == otra.get_email_empresarial())
+
+def test_compare_usuario_admin():
+	t1 = TestUsuarioAdmin("Carlos", "carlos7ma@gmail.com", "75925767-F", "carlos@medauth.com")
+	t2 = TestUsuarioAdmin("Carlos", "carlos7ma@gmail.com", "75925767-F", "carlos@medauth.com")
+	t3 = TestUsuarioAdmin("Carlos", "prueba@gmail.com", "75925767-F", "carlos@medauth.com")
+	assert t1 == t1 # Pasa test
+	assert t1 == t2 # Pasa test
+	assert t2 == t3 # No pasa test
