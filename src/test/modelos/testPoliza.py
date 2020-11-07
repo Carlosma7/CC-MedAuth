e9a72291cda20a656500e8567303f5774025679d
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-11-07 12:31:44 +0100

    #34 #35 #58 Creación test modelo póliza

---
+++ src/test/modelos/testPoliza.py
@@ -0,0 +1,67 @@
+from .testUsuarioCliente import TestUsuarioCliente
+from typing import List
+import datetime
+
+
+class TestPoliza:
+    def __init__(self, usuario: TestUsuarioCliente, periodo_carencia: datetime, tipo: str, 
+    			copagos: float, mensualidad: str, servicios_excluidos: List[str]):
+        self.__titular = usuario
+        self.__periodo_carencia = periodo_carencia
+        self.__tipo = tipo
+        self.__copagos = copagos
+        self.__mensualidad = mensualidad
+        self.__servicios_excluidos = servicios_excluidos[:]
+
+    def get_titular(self):
+    	return self.__titular
+
+    def get_periodo_carencia(self):
+    	return self.__periodo_carencia
+
+    def get_tipo(self):
+    	return self.__tipo
+
+    def set_tipo(self, tipo):
+    	self.__tipo = tipo
+
+    def get_copagos(self):
+    	return self.__copagos
+
+    def set_copagos(self, copagos):
+    	self.__copagos = copagos
+
+    def get_mensualidad(self):
+    	return self.__mensualidad
+
+    def set_mensualidad(self, mensualidad):
+    	self.__mensualidad = mensualidad
+
+    def get_servicios_excluidos(self):
+    	return self.__servicios_excluidos
+
+    def set_servicios_excluidos(self, servicios_excluidos):
+    	self.__servicios_excluidos = servicios_excluidos[:]
+    
+    def __eq__(self, otra):
+    	assert self.__titular == otra.get_titular()
+    	assert self.__periodo_carencia == otra.get_periodo_carencia()
+    	assert self.__tipo == otra.get_tipo()
+    	assert self.__copagos == otra.get_copagos()
+    	assert self.__mensualidad == otra.get_mensualidad()
+    	assert self.__servicios_excluidos == otra.get_servicios_excluidos()
+    	
+    	return (self.__titular == otra.get_titular() ) and (self.__periodo_carencia == otra.get_periodo_carencia()) and (self.__tipo == otra.get_tipo()) and (self.__copagos == otra.get_copagos()) and (self.__mensualidad == otra.get_mensualidad()) and (self.__servicios_excluidos == otra.get_servicios_excluidos())
+
+def test_compare_poliza():
+	u = TestUsuarioCliente("Carlos", "carlos7ma@gmail.com", "75925767-F", "ES12345678", "12345678")
+	u2 = TestUsuarioCliente("Carlos", "carlos7ma@gmail.com", "75925767-F", "ES12345678", "12345678")
+	fecha = datetime.datetime(2020, 5, 17)
+	fecha2 = datetime.datetime(2020, 5, 18)
+	
+	t1 = TestPoliza(u, fecha, "Total", 35.99, 103.0, ["TAC", "Apendicitis"])
+	t2 = TestPoliza(u2, fecha, "Total", 35.99, 103.0, ["TAC", "Apendicitis"])
+	t3 = TestPoliza(u2, fecha2, "Básico", 35.99, 103.0, ["TAC", "Apendicitis"])
+	assert t1 == t1 # Pasa test
+	assert t1 == t2 # Pasa test
+	assert t2 == t3 # No pasa test
