b4d494c6e7f9ee43556a64d41eb083d50280de1f
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-11-07 17:57:44 +0100

    #36 #57 #58 Se añade el modelo de test de Prescripción a la biblioteca

--- src/test/modelos/__init__.py
+++ src/test/modelos/__init__.py
@@ -2,3 +2,4 @@ from .testUsuario import TestUsuario
 from .testUsuarioAdmin import TestUsuarioAdmin
 from .testUsuarioCliente import TestUsuarioCliente
 from .testPoliza import TestPoliza
+from .testPrescripcion import TestPrescripcion
