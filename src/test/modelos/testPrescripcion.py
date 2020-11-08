e8c4420e8d2d81f3c369525befefc23c88073c46
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-11-08 01:18:19 +0100

    #36 #58 Comentarios clase de prescripción médica

--- src/test/modelos/testPrescripcion.py
+++ src/test/modelos/testPrescripcion.py
@@ -3,7 +3,7 @@ from .especialidad import Especialidad
 from typing import List
 import datetime
 
-
+# Clase de prescripción médica
 class TestPrescripcion:
 	def __init__(self, id_prescripcion: str, asegurado: TestUsuarioCliente, id_poliza: str, fecha_realizacion: datetime, especialidad: Especialidad, facultativo_prescriptor: str, facultativo_realizador: str, servicios_solicitados: List[str], consulta: str):
 		self.__id_prescripcion = id_prescripcion
@@ -16,8 +16,9 @@ class TestPrescripcion:
 		self.__servicios_solicitados = servicios_solicitados[:]
 		self.__consulta = consulta
 
-		assert isinstance(self.__especialidad, Especialidad)
+		assert isinstance(self.__especialidad, Especialidad) # Comprobamos que pertenece al enum Especialidad médica
 
+	# Métodos get/set
 	def get_id_prescripcion(self):
 		return self.__id_prescripcion
 
@@ -45,6 +46,7 @@ class TestPrescripcion:
 	def get_consulta(self):
 		return self.__consulta
 
+	# Override método equal
 	def __eq__(self, otra):
 		assert self.__id_prescripcion == otra.get_id_prescripcion()
 		assert self.__asegurado == otra.get_asegurado()
@@ -58,12 +60,12 @@ class TestPrescripcion:
     	
 		return ((self.__id_prescripcion == otra.get_id_prescripcion()) and (self.__asegurado == otra.get_asegurado()) and (self.__id_poliza == otra.get_id_poliza()) and (self.__fecha_realizacion == otra.get_fecha_realizacion()) and (self.__especialidad == otra.get_especialidad()) and (self.__facultativo_prescriptor == otra.get_facultativo_prescriptor()) and (self.__facultativo_realizador == otra.get_facultativo_realizador()) and (self.__servicios_solicitados == otra.get_servicios_solicitados()) and (self.__consulta == otra.get_consulta()))
 
+# Test comparación de prescripciones
 def test_compare_prescripcion():
 	u = TestUsuarioCliente("Carlos", "carlos7ma@gmail.com", "75925767-F", "ES12345678", "12345678")
 	fecha = datetime.datetime(2020, 5, 17)
 	
 	t1 = TestPrescripcion("PR-12345678", u, "MA-75925767-1", fecha, Especialidad.Traumatologia, "D. Fernando", "D. Juan", ["Radiografía", "Ortopedia"], "Centro médico capital, Sala 2")
 	t2 = TestPrescripcion("PR-12345678", u, "MA-75925767-1", fecha, Especialidad.Traumatologia, "D. Fernando", "D. Juan", ["Radiografía", "Ortopedia"], "Centro médico capital, Sala 2")
-	
 	assert t1 == t1 # Pasa test
 	assert t1 == t2 # Pasa test
