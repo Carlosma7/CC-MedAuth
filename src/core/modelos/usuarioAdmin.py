459015f61900bc5e123e3e41dbe5c16026c5fea2
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-11-08 01:59:34 +0100

    #43 Definición modelo usuario administrativo

--- src/core/modelos/usuarioAdmin.py
+++ src/core/modelos/usuarioAdmin.py
@@ -1,9 +1,35 @@
-from .usuario import Usuario
+from .Usuario import TestUsuario
 
-
+# Clase de usuario administrativo
 class UsuarioAdmin(Usuario):
 	def __init__(self, nombre: str, email: str, dni: str, email_empresarial: str):
-		self.nombre = nombre
-		self.email = email
-		self.dni = dni
-		self.email_empresarial = email_empresarial
+		self.__nombre = nombre
+		self.__email = email
+		self.__dni = dni
+		self.__email_empresarial = email_empresarial
+	
+	# Métodos get/set
+	def get_nombre(self):
+		return self.__nombre
+	
+	def set_nombre(self, nombre):
+		self.__nombre = nombre
+		
+	def get_email(self):
+		return self.__email
+		
+	def set_email(self, email):
+		self.__email = email
+	
+	def get_dni(self):
+		return self.__dni
+		
+	def get_email_empresarial(self):
+		return self.__email_empresarial
+	
+	def set_email_empresarial(self, email_empresarial):
+		self.__email_empresarial = email_empresarial
+	
+	# Override método equal
+	def __eq__(self, otra):
+		return (self.__nombre == otra.get_nombre() ) and (self.__email == otra.get_email()) and (self.__dni == otra.get_dni()) and (self.__email_empresarial == otra.get_email_empresarial())
