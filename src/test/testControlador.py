4ce9c7a109775bcb7cd2989a9bcccd1a3d6f395a
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-11-08 01:36:30 +0100

    35 #58 Comentarios administración de pólizas: Creación, modificación y desactivación

--- src/test/testControlador.py
+++ src/test/testControlador.py
@@ -89,7 +89,7 @@ class TestController:
 		# Obtengo el usuario
 		len_antes = len(self.usuarios)
 		usuario = [c for c in self.usuarios if c.get_dni() == dni][0]
-		
+
 		# Elimino el usuario
 		self.usuarios.remove(usuario)
 
@@ -98,29 +98,41 @@ class TestController:
 	
 	# [HU4] Administrar póliza: Crear una póliza
 	def crear_poliza(self, dni: str, periodo_carencia: datetime, tipo: TipoPoliza, copagos: float, mensualidad: str, servicios_excluidos: List[str], modulos_extra: List[ModuloExtra]):
+		# Obtengo el usuario cliente/asegurado por su dni
 		cliente = [c for c in self.usuarios if c.get_dni() == dni][0]
 		
+		# Compruebo que no tiene actualmente ninguna póliza activa
 		poliza_activa = [p for p in self.polizas if p.get_id_poliza() == cliente.get_id_poliza() and p.get_activa() == True]
 		assert len(poliza_activa) == 0
 		
+		# Compongo el identificador de la póliza con el formato MA-DNI-ID_ULTIMA_POLIZA+1
 		id_poliza = "MA-" + dni[:9]
+		# Obtengo las polizas previas
 		polizas_previas = [p for p in self.polizas if p.get_id_poliza()[:12] == id_poliza]
+		
 		if len(polizas_previas) > 0:
+			# Si posee polizas previas canceladas, obtengo el ID de la última que tuvo
 			id_poliza = id_poliza + str(int(polizas_previas[-1][-1]) + 1)
 		else:
+			# Si es la primera se crea como tal
 			id_poliza = id_poliza + "1"
 
+		# Compruebo que no existe una póliza con el identificador que se acaba de generar
 		polizas = [p for p in self.polizas if p.get_id_poliza() == id_poliza]
 		assert len(polizas) == 0
-			
-		cliente.set_id_poliza(id_poliza)
-		assert cliente.get_id_poliza() == id_poliza
-		
+
+		# Se crea la póliza y se almacena
 		p = TestPoliza(cliente, id_poliza, periodo_carencia, tipo, copagos, mensualidad, servicios_excluidos, modulos_extra, True)
 		len_antes = len(self.polizas)
 		self.polizas.append(p)
+		# Se comprueba que a póliza se ha almacenado
 		assert len(self.polizas) > len_antes
+		
+		# Se le asigna la póliza al cliente/asegurado y se comprueba
+		cliente.set_id_poliza(id_poliza)
+		assert cliente.get_id_poliza() == id_poliza
 
+		# Se busca la póliza y se comprueba que se ha almacenado correctamente
 		poliza = [p for p in self.polizas if p.get_id_poliza() == id_poliza][0]
 		assert poliza.get_titular() == cliente
 		assert poliza.get_id_poliza() == id_poliza
@@ -133,13 +145,18 @@ class TestController:
 		
 	# [HU4] Administrar póliza: Modificar una póliza
 	def modificar_poliza(self, dni: str, periodo_carencia: datetime, tipo: TipoPoliza, copagos: float, mensualidad: float, servicios_excluidos: List[str], modulos_extra: List[ModuloExtra]):
+		# Se obtiene el usuario cliente/asegurado por su dni
 		cliente = [c for c in self.usuarios if c.get_dni() == dni][0]
-		id_poliza = cliente.get_id_poliza()
+		# Se comprueba que el usuario es correcto
 		assert cliente.get_dni() == dni
+		# Se obtiene su póliza activa
+		id_poliza = cliente.get_id_poliza()
 		
+		# Se obtiene la póliza asociada al identificador y se comprueba
 		poliza = [p for p in self.polizas if p.get_id_poliza() == id_poliza][0]
 		assert poliza.get_id_poliza() == id_poliza
 		
+		# Modificación de la póliza
 		poliza.set_periodo_carencia(periodo_carencia)
 		poliza.set_tipo(tipo)
 		poliza.set_copagos(copagos)
@@ -147,6 +164,7 @@ class TestController:
 		poliza.set_servicios_excluidos(servicios_excluidos)
 		poliza.set_modulos_extra(modulos_extra)
 		
+		# Comprobación de modificación correcta de la póliza
 		assert poliza.get_periodo_carencia() == periodo_carencia
 		assert poliza.get_tipo() == tipo
 		assert poliza.get_copagos() == copagos
@@ -157,14 +175,20 @@ class TestController:
 	
 	# [HU4] Administrar póliza: Desactivar una póliza
 	def desactivar_poliza(self, dni: str):
+		# Se obtiene el usuario cliente/asegurado por su dni
 		cliente = [c for c in self.usuarios if c.get_dni() == dni][0]
+		# Se obtiene el identificador de la póliza activa del cliente/asegurado
 		id_poliza = cliente.get_id_poliza()
+
+		# Se obtiene la póliza y se comprueba
 		poliza = [p for p in self.polizas if p.get_id_poliza() == id_poliza][0]
 		assert poliza.get_id_poliza() == cliente.get_id_poliza()
 		
+		# Se anula el identificador de la póliza para el cliente y se comprueba
 		cliente.set_id_poliza("")
 		assert cliente.get_id_poliza() == ""
 		
+		# Se desactiva la póliza y se comprueba
 		poliza.set_activa(False)
 		assert poliza.get_activa() == False
 	
