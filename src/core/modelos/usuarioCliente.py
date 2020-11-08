b10568c7529fc4aa4a4db2b11ee1787ae005e3c8
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-11-08 02:01:15 +0100

    #44 Definición modelo usuario cliente/asegurado

--- src/core/modelos/usuarioCliente.py
+++ src/core/modelos/usuarioCliente.py
@@ -1,10 +1,42 @@
-from .usuario import Usuario
+from .Usuario import Usuario
 
-
+# Clase de usuario cliente/asegurado
 class UsuarioCliente(Usuario):
 	def __init__(self, nombre: str, email: str, dni: str, cuenta_bancaria: str, id_poliza: str):
-		self.nombre = nombre
-		self.email = email
-		self.dni = dni
-		self.cuenta_bancaria = cuenta_bancaria
-		self.id_poliza = id_poliza
+		self.__nombre = nombre
+		self.__email = email
+		self.__dni = dni
+		self.__cuenta_bancaria = cuenta_bancaria
+		self.__id_poliza = id_poliza
+	
+	# Métodos get/set
+	def get_nombre(self):
+		return self.__nombre
+		
+	def set_nombre(self, nombre):
+		self.__nombre = nombre
+		
+	def get_email(self):
+		return self.__email
+		
+	def set_email(self, email):
+		self.__email = email
+	
+	def get_dni(self):
+		return self.__dni
+		
+	def get_cuenta_bancaria(self):
+		return self.__cuenta_bancaria
+		
+	def set_cuenta_bancaria(self, cuenta_bancaria):
+		self.__cuenta_bancaria = cuenta_bancaria
+		
+	def get_id_poliza(self):
+		return self.__id_poliza
+	
+	def set_id_poliza(self, id_poliza):
+		self.__id_poliza = id_poliza
+	
+	# Override método equal
+	def __eq__(self, otra):
+		return (self.__nombre == otra.get_nombre() ) and (self.__email == otra.get_email()) and (self.__dni == otra.get_dni()) and (self.__cuenta_bancaria == otra.get_cuenta_bancaria() and (self.__id_poliza == otra.get_id_poliza()))
