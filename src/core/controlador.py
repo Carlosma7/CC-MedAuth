e60e6ae68c3af5e006754b7d4a5a3a0eb445f754
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-11-08 02:16:48 +0100

    #35 Se añaden las funcionalidades de administración de pólizas: Crear, modificar y desactivar una póliza

--- src/core/controlador.py
+++ src/core/controlador.py
@@ -55,3 +55,68 @@ class Controller:
 
 		# Se elimina el usuario
 		self.usuarios.remove(usuario)
+
+	# [HU4] Administrar póliza: Crear una póliza
+	def crear_poliza(self, dni: str, periodo_carencia: datetime, tipo: TipoPoliza, copagos: float, mensualidad: str, servicios_excluidos: List[str], modulos_extra: List[ModuloExtra]):
+		# Se obtiene el usuario cliente/asegurado por su dni
+		cliente = [c for c in self.usuarios if c.get_dni() == dni][0]
+		
+		# Se comprueba que no tiene actualmente ninguna póliza activa
+		poliza_activa = [p for p in self.polizas if p.get_id_poliza() == cliente.get_id_poliza() and p.get_activa() == True]
+		assert len(poliza_activa) == 0
+		
+		# Se compone el identificador de la póliza con el formato MA-DNI-ID_ULTIMA_POLIZA+1
+		id_poliza = "MA-" + dni[:9]
+		# Se obtienen las polizas previas
+		polizas_previas = [p for p in self.polizas if p.get_id_poliza()[:12] == id_poliza]
+		
+		if len(polizas_previas) > 0:
+			# Si posee polizas previas canceladas, se obtiene el ID de la última que tuvo
+			id_poliza = id_poliza + str(int(polizas_previas[-1][-1]) + 1)
+		else:
+			# Si es la primera se crea como tal
+			id_poliza = id_poliza + "1"
+
+		# Se crea la póliza y se almacena
+		p = TestPoliza(cliente, id_poliza, periodo_carencia, tipo, copagos, mensualidad, servicios_excluidos, modulos_extra, True)
+
+		self.polizas.append(p)
+		
+		# Se le asigna la póliza al cliente/asegurado
+		cliente.set_id_poliza(id_poliza)
+		
+	# [HU4] Administrar póliza: Modificar una póliza
+	def modificar_poliza(self, dni: str, periodo_carencia: datetime, tipo: TipoPoliza, copagos: float, mensualidad: float, servicios_excluidos: List[str], modulos_extra: List[ModuloExtra]):
+		# Se obtiene el usuario cliente/asegurado por su dni
+		cliente = [c for c in self.usuarios if c.get_dni() == dni][0]
+		
+		# Se obtiene su póliza activa
+		id_poliza = cliente.get_id_poliza()
+		
+		# Se obtiene la póliza asociada al identificador
+		poliza = [p for p in self.polizas if p.get_id_poliza() == id_poliza][0]
+		
+		# Modificación de la póliza
+		poliza.set_periodo_carencia(periodo_carencia)
+		poliza.set_tipo(tipo)
+		poliza.set_copagos(copagos)
+		poliza.set_mensualidad(mensualidad)
+		poliza.set_servicios_excluidos(servicios_excluidos)
+		poliza.set_modulos_extra(modulos_extra)
+	
+	# [HU4] Administrar póliza: Desactivar una póliza
+	def desactivar_poliza(self, dni: str):
+		# Se obtiene el usuario cliente/asegurado por su dni
+		cliente = [c for c in self.usuarios if c.get_dni() == dni][0]
+
+		# Se obtiene el identificador de la póliza activa del cliente/asegurado
+		id_poliza = cliente.get_id_poliza()
+
+		# Se obtiene la póliza
+		poliza = [p for p in self.polizas if p.get_id_poliza() == id_poliza][0]
+		
+		# Se anula el identificador de la póliza para el cliente
+		cliente.set_id_poliza("")
+		
+		# Se desactiva la póliza
+		poliza.set_activa(False)
