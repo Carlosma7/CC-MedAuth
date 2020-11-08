c1bf8d94ae0d4236ce9a773c51a7a389c60866ac
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-11-08 01:13:39 +0100

    #43 #58 Comentarios clase de usuario administrativo

--- src/test/modelos/testUsuarioAdmin.py
+++ src/test/modelos/testUsuarioAdmin.py
@@ -1,14 +1,15 @@
 from .testUsuario import TestUsuario
 import pytest
 
-
+# Clase de usuario administrativo
 class TestUsuarioAdmin(TestUsuario):
 	def __init__(self, nombre: str, email: str, dni: str, email_empresarial: str):
 		self.__nombre = nombre
 		self.__email = email
 		self.__dni = dni
 		self.__email_empresarial = email_empresarial
-		
+	
+	# Métodos get/set
 	def get_nombre(self):
 		return self.__nombre
 	
@@ -30,6 +31,7 @@ class TestUsuarioAdmin(TestUsuario):
 	def set_email_empresarial(self, email_empresarial):
 		self.__email_empresarial = email_empresarial
 	
+	# Override método equal
 	def __eq__(self, otra):
 		assert self.__nombre == otra.get_nombre()
 		assert self.__email == otra.get_email()
@@ -38,10 +40,9 @@ class TestUsuarioAdmin(TestUsuario):
 		
 		return (self.__nombre == otra.get_nombre() ) and (self.__email == otra.get_email()) and (self.__dni == otra.get_dni()) and (self.__email_empresarial == otra.get_email_empresarial())
 
+# Test comparación usuario administrativo
 def test_compare_usuario_admin():
 	t1 = TestUsuarioAdmin("Carlos", "carlos7ma@gmail.com", "75925767-F", "carlos@medauth.com")
 	t2 = TestUsuarioAdmin("Carlos", "carlos7ma@gmail.com", "75925767-F", "carlos@medauth.com")
-	t3 = TestUsuarioAdmin("Carlos", "prueba@gmail.com", "75925767-F", "carlos@medauth.com")
 	assert t1 == t1 # Pasa test
 	assert t1 == t2 # Pasa test
-	assert t2 == t3 # No pasa test
