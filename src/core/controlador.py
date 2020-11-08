1ae45f9fdb7804be93243c79b4dad17017aa6c4a
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-11-08 02:08:39 +0100

    #55 Se añaden las funcionalidades de administración de usuarios: Modificación y eliminación

--- src/core/controlador.py
+++ src/core/controlador.py
@@ -26,3 +26,32 @@ class Controller:
 
 		# Se almacena
 		self.usuarios.append(c)
+
+	# [HU3] Administrar usuario: Modificación administrador
+	def modificar_admin(self, nombre: str, email: str, dni: str):
+		# Se obtiene el usuario administrativo por su dni
+		admin = [c for c in self.usuarios if c.get_dni() == dni][0]
+		
+		# Se modifica la información
+		admin.set_nombre(nombre)
+		admin.set_email(email)
+		email_empresarial = email.split('@')[0] + '@medauth.com'
+		admin.set_email_empresarial(email_empresarial)
+
+	# [HU3] Administrar usuario: Modificación cliente
+	def modificar_cliente(self, nombre: str, email: str, dni: str, cuenta_bancaria: str):
+		# Se obtiene el usuario cliente/asegurado por su dni
+		cliente = [c for c in self.usuarios if c.get_dni() == dni][0]
+		
+		# Se modifica la información
+		cliente.set_nombre(nombre)
+		cliente.set_email(email)
+		cliente.set_cuenta_bancaria(cuenta_bancaria)
+	
+	# [HU3] Administrar usuario: Eliminar usuario
+	def eliminar_usuario(self, dni: str):
+		# Se obtiene el usuario por su dni
+		usuario = [c for c in self.usuarios if c.get_dni() == dni][0]
+
+		# Se elimina el usuario
+		self.usuarios.remove(usuario)
