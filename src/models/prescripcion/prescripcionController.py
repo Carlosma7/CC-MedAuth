08a8a36844c1d885dc86a6a3c40a40e403c57ba8
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-11-08 02:50:29 +0100

    #54 Se elimina la anterior organización de los modelos tras realizar el nuevo core

--- src/models/prescripcion/prescripcionController.py
+++
@@ -1,9 +0,0 @@
-from .prescripcion import Prescripcion
-from typing import List
-
-class PrescripcionController:
-
-	self.prescripciones: List[Prescripcion]
-	
-	def upload_prescripcion(self):
-		return
