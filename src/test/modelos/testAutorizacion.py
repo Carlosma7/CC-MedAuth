63ed5684a84d1713fe9d60ec46912620c452dd24
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-11-08 01:02:43 +0100

    #37 #38 #39 #40 #58 Se cambia el valor estado por aceptada para mayor sencillez, y además se añade el campo especialidad médica

--- src/test/modelos/testAutorizacion.py
+++ src/test/modelos/testAutorizacion.py
@@ -1,20 +1,24 @@
 from .testUsuarioCliente import TestUsuarioCliente
+from .especialidad import Especialidad
 from typing import List
 import datetime
 
 
 class TestAutorizacion:
-	def __init__(self, id_autorizacion: str, asegurado: TestUsuarioCliente, id_prescripcion: str, id_poliza: str, estado: str, motivo_rechazo: str, fecha_realizacion: datetime, servicios_aceptados: List[str], facultativo_realizador: str, consulta: str):
+	def __init__(self, id_autorizacion: str, asegurado: TestUsuarioCliente, id_prescripcion: str, id_poliza: str, aceptada: bool, motivo_rechazo: str, fecha_realizacion: datetime, especialidad: Especialidad, servicios_aceptados: List[str], facultativo_realizador: str, consulta: str):
 		self.__id_autorizacion = id_autorizacion
 		self.__asegurado = asegurado
 		self.__id_prescripcion = id_prescripcion
 		self.__id_poliza = id_poliza
-		self.__estado = estado
+		self.__aceptada = aceptada
 		self.__motivo_rechazo = motivo_rechazo
 		self.__fecha_realizacion = fecha_realizacion
+		self.__especialidad = especialidad
 		self.__servicios_aceptados = servicios_aceptados
 		self.__facultativo_realizador = facultativo_realizador
 		self.__consulta = consulta
+
+		assert isinstance(self.__especialidad, Especialidad)
 
 	def get_id_autorizacion(self):
 		return self.__id_autorizacion
@@ -28,11 +32,11 @@ class TestAutorizacion:
 	def get_id_poliza(self):
 		return self.__id_poliza
 
-	def get_estado(self):
-		return self.__estado
+	def get_aceptada(self):
+		return self.__aceptada
 
-	def set_estado(self, estado):
-		self.__estado = estado
+	def set_aceptada(self, aceptada):
+		self.__aceptada = aceptada
 
 	def get_motivo_rechazo(self):
 		return self.__motivo_rechazo
@@ -69,21 +73,21 @@ class TestAutorizacion:
 		assert self.__asegurado == otra.get_asegurado()
 		assert self.__id_prescripcion == otra.get_id_prescripcion()
 		assert self.__id_poliza == otra.get_id_poliza()
-		assert self.__estado == otra.get_estado()
+		assert self.__aceptada == otra.get_aceptada()
 		assert self.__motivo_rechazo == otra.get_motivo_rechazo()
 		assert self.__fecha_realizacion == otra.get_fecha_realizacion()
 		assert self.__servicios_aceptados == otra.get_servicios_aceptados()
 		assert self.__facultativo_realizador == otra.get_facultativo_realizador()
 		assert self.__consulta == otra.get_consulta()
 
-		return ((self.__id_autorizacion == otra.get_id_autorizacion()) and (self.__asegurado == otra.get_asegurado()) and (self.__id_prescripcion == otra.get_id_prescripcion()) and (self.__id_poliza == otra.get_id_poliza()) and (self.__estado == otra.get_estado()) and (self.__motivo_rechazo == otra.get_motivo_rechazo()) and (self.__fecha_realizacion == otra.get_fecha_realizacion()) and (self.__servicios_aceptados == otra.get_servicios_aceptados()) and (self.__facultativo_realizador == otra.get_facultativo_realizador()) and (self.__consulta == otra.get_consulta()))
+		return ((self.__id_autorizacion == otra.get_id_autorizacion()) and (self.__asegurado == otra.get_asegurado()) and (self.__id_prescripcion == otra.get_id_prescripcion()) and (self.__id_poliza == otra.get_id_poliza()) and (self.__aceptada == otra.get_aceptada()) and (self.__motivo_rechazo == otra.get_motivo_rechazo()) and (self.__fecha_realizacion == otra.get_fecha_realizacion()) and (self.__servicios_aceptados == otra.get_servicios_aceptados()) and (self.__facultativo_realizador == otra.get_facultativo_realizador()) and (self.__consulta == otra.get_consulta()))
 		
 def test_compare_autorizacion():
 	u = TestUsuarioCliente("Carlos", "carlos7ma@gmail.com", "75925767-F", "ES12345678", "12345678")
 	fecha = datetime.datetime(2020, 5, 17)
 	
-	t1 = TestAutorizacion("AU-12345678", u, "MA-75925767-1", "PR-12345678", "Aceptada", "", fecha, ["Radiografía", "Ortopedia"], "D. Manuel", "Centro médico capital, Sala 2")
-	t2 = TestAutorizacion("AU-12345678", u, "MA-75925767-1", "PR-12345678", "Aceptada", "", fecha, ["Radiografía", "Ortopedia"], "D. Manuel", "Centro médico capital, Sala 2")
+	t1 = TestAutorizacion("AU-12345678", u, "MA-75925767-1", "PR-12345678", True, "", fecha, Especialidad.Traumatologia, ["Radiografía", "Ortopedia"], "D. Manuel", "Centro médico capital, Sala 2")
+	t2 = TestAutorizacion("AU-12345678", u, "MA-75925767-1", "PR-12345678", True, "", fecha, Especialidad.Traumatologia, ["Radiografía", "Ortopedia"], "D. Manuel", "Centro médico capital, Sala 2")
 	
 	assert t1 == t1 # Pasa test
 	assert t1 == t2 # Pasa test
