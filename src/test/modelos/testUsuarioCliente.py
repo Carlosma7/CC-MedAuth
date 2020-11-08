dce4fd76509895d69a74eea7fe6426ccd69fa9bc
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-11-08 01:14:53 +0100

    #44 #58 Comentarios clase de usuario cliente/asegurado

--- src/test/modelos/testUsuarioCliente.py
+++ src/test/modelos/testUsuarioCliente.py
@@ -1,7 +1,7 @@
 from .testUsuario import TestUsuario
 import pytest
 
-
+# Clase de usuario cliente/asegurado
 class TestUsuarioCliente(TestUsuario):
 	def __init__(self, nombre: str, email: str, dni: str, cuenta_bancaria: str, id_poliza: str):
 		self.__nombre = nombre
@@ -9,7 +9,8 @@ class TestUsuarioCliente(TestUsuario):
 		self.__dni = dni
 		self.__cuenta_bancaria = cuenta_bancaria
 		self.__id_poliza = id_poliza
-		
+	
+	# Métodos get/set
 	def get_nombre(self):
 		return self.__nombre
 		
@@ -37,6 +38,7 @@ class TestUsuarioCliente(TestUsuario):
 	def set_id_poliza(self, id_poliza):
 		self.__id_poliza = id_poliza
 	
+	# Override método equal
 	def __eq__(self, otra):
 		assert self.__nombre == otra.get_nombre()
 		assert self.__email == otra.get_email()
@@ -46,10 +48,9 @@ class TestUsuarioCliente(TestUsuario):
 		
 		return (self.__nombre == otra.get_nombre() ) and (self.__email == otra.get_email()) and (self.__dni == otra.get_dni()) and (self.__cuenta_bancaria == otra.get_cuenta_bancaria() and (self.__id_poliza == otra.get_id_poliza()))
 
+# Test comparación usuario cliente/asegurado
 def test_compare_usuario_cliente():
 	t1 = TestUsuarioCliente("Carlos", "carlos7ma@gmail.com", "75925767-F", "ES12345678", "12345678")
 	t2 = TestUsuarioCliente("Carlos", "carlos7ma@gmail.com", "75925767-F", "ES12345678", "12345678")
-	t3 = TestUsuarioCliente("Carlos", "prueba@gmail.com", "75925767-F", "ES12345678", "12345678")
 	assert t1 == t1 # Pasa test
 	assert t1 == t2 # Pasa test
-	assert t2 == t3 # No pasa test
