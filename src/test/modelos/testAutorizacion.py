f369747e4818d7043fd549c282c882e348e8b6b4
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-11-08 01:20:13 +0100

    #37 #38 #39 #40 #58 Comentarios clase de autorización médica

--- src/test/modelos/testAutorizacion.py
+++ src/test/modelos/testAutorizacion.py
@@ -3,7 +3,7 @@ from .especialidad import Especialidad
 from typing import List
 import datetime
 
-
+# Clase de autorización médica
 class TestAutorizacion:
 	def __init__(self, id_autorizacion: str, asegurado: TestUsuarioCliente, id_prescripcion: str, id_poliza: str, aceptada: bool, motivo_rechazo: str, fecha_realizacion: datetime, especialidad: Especialidad, servicios_aceptados: List[str], facultativo_realizador: str, consulta: str):
 		self.__id_autorizacion = id_autorizacion
@@ -18,8 +18,9 @@ class TestAutorizacion:
 		self.__facultativo_realizador = facultativo_realizador
 		self.__consulta = consulta
 
-		assert isinstance(self.__especialidad, Especialidad)
+		assert isinstance(self.__especialidad, Especialidad) # Comprobamos que pertenece al enum Especialidad médica
 
+	# Métodos get/set
 	def get_id_autorizacion(self):
 		return self.__id_autorizacion
 
@@ -74,6 +75,7 @@ class TestAutorizacion:
 	def set_consulta(self, consulta):
 		self.__consulta = consulta
 
+	# Override método equal
 	def __eq__(self, otra):
 		assert self.__id_autorizacion == otra.get_id_autorizacion()
 		assert self.__asegurado == otra.get_asegurado()
@@ -88,13 +90,13 @@ class TestAutorizacion:
 		assert self.__consulta == otra.get_consulta()
 
 		return ((self.__id_autorizacion == otra.get_id_autorizacion()) and (self.__asegurado == otra.get_asegurado()) and (self.__id_prescripcion == otra.get_id_prescripcion()) and (self.__id_poliza == otra.get_id_poliza()) and (self.__aceptada == otra.get_aceptada()) and (self.__motivo_rechazo == otra.get_motivo_rechazo()) and (self.__fecha_realizacion == otra.get_fecha_realizacion()) and (self.__especialidad == otra.get_especialidad()) and (self.__servicios_aceptados == otra.get_servicios_aceptados()) and (self.__facultativo_realizador == otra.get_facultativo_realizador()) and (self.__consulta == otra.get_consulta()))
-		
+
+# Test comparación de autorizaciones	
 def test_compare_autorizacion():
 	u = TestUsuarioCliente("Carlos", "carlos7ma@gmail.com", "75925767-F", "ES12345678", "12345678")
 	fecha = datetime.datetime(2020, 5, 17)
 	
 	t1 = TestAutorizacion("AU-12345678", u, "MA-75925767-1", "PR-12345678", True, "", fecha, Especialidad.Traumatologia, ["Radiografía", "Ortopedia"], "D. Manuel", "Centro médico capital, Sala 2")
 	t2 = TestAutorizacion("AU-12345678", u, "MA-75925767-1", "PR-12345678", True, "", fecha, Especialidad.Traumatologia, ["Radiografía", "Ortopedia"], "D. Manuel", "Centro médico capital, Sala 2")
-	
 	assert t1 == t1 # Pasa test
 	assert t1 == t2 # Pasa test
