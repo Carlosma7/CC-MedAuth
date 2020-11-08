d822fb721f020420a82e2c6e8440b613fc85040e
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-11-08 12:53:10 +0100

    #61 Definición de gestor de tareas Invoke y requisitos de instalación del proyecto en requirements.txt

--- tasks.py
+++ tasks.py
@@ -1,13 +1,41 @@
 from invoke import task, run
 
+# Tarea de limpieza de ficheros
 @task 
 def clean(c):
-    print("Borrando caché de pytest")
-    run ("rm -d -r .pytest_cache")
-    run ("rm -d -r ./tests/.pytest_cache")
-    run ("rm -d -r ./tests/__pycache__")
-
+	print("Borrando caché de python.")
+	run("find . -maxdepth 5 -type d -name __pycache__ -exec rm -r {} +")
+	print("Borrando caché de pytest.")
+	run("find . -maxdepth 5 -type d -name .pytest_cache -exec rm -r {} +")
+	
+# Tarea de instalación de dependencias
 @task
-def build(c):
-    run ('pip3 install -r requirements.txt')
-    print ('Instalación completada.')
+def install(c):
+	run ('pip3 install -r requirements.txt')
+	print ('Instalación completada.')
+
+# Tarea de ejecución de tests
+@task
+def test(c):
+	print("Ejecución de test.\n")
+	print("Test Clase UsuarioAdmin:")
+	run("pytest -v --disable-pytest-warnings ./src/test/modelos/testUsuarioAdmin.py")
+	
+	print("\nTest Clase UsuarioCliente:")
+	run("pytest -v --disable-pytest-warnings ./src/test/modelos/testUsuarioCliente.py")
+	
+	print("\nTest Clase Poliza:")
+	run("pytest -v --disable-pytest-warnings ./src/test/modelos/testPoliza.py")
+	
+	print("\nTest Clase Prescripcion:")
+	run("pytest -v --disable-pytest-warnings ./src/test/modelos/testPrescripcion.py")
+	
+	print("\nTest Clase Autorizacion:")
+	run("pytest -v --disable-pytest-warnings ./src/test/modelos/testAutorizacion.py")
+	
+	print("\nTest Clase Cita:")
+	run("pytest -v --disable-pytest-warnings ./src/test/modelos/testCita.py")
+	
+	print("\nTest Clase Controlador:")
+	run("pytest -v --disable-pytest-warnings ./src/test/testControlador.py")
+
