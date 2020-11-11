ffb7e044fb9f34784ee24fb64c5be3a5c7160dd7
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-11-11 20:51:39 +0100

    #58 Definición test usuario cliente/asegurado

--- src/test/testUsuarioCliente.py
+++ src/test/testUsuarioCliente.py
@@ -1,56 +1,15 @@
-from testUsuario import TestUsuario
+import sys
+import os
+sys.path.append(os.path.abspath('./core/'))
+
+from usuarioCliente import UsuarioCliente
+
 import pytest
 
-# Clase de usuario cliente/asegurado
-class TestUsuarioCliente(TestUsuario):
-	def __init__(self, nombre: str, email: str, dni: str, cuenta_bancaria: str, id_poliza: str):
-		self.__nombre = nombre
-		self.__email = email
-		self.__dni = dni
-		self.__cuenta_bancaria = cuenta_bancaria
-		self.__id_poliza = id_poliza
-	
-	# Métodos get/set
-	def get_nombre(self):
-		return self.__nombre
-		
-	def set_nombre(self, nombre):
-		self.__nombre = nombre
-		
-	def get_email(self):
-		return self.__email
-		
-	def set_email(self, email):
-		self.__email = email
-	
-	def get_dni(self):
-		return self.__dni
-		
-	def get_cuenta_bancaria(self):
-		return self.__cuenta_bancaria
-		
-	def set_cuenta_bancaria(self, cuenta_bancaria):
-		self.__cuenta_bancaria = cuenta_bancaria
-		
-	def get_id_poliza(self):
-		return self.__id_poliza
-	
-	def set_id_poliza(self, id_poliza):
-		self.__id_poliza = id_poliza
-	
-	# Override método equal
-	def __eq__(self, otra):
-		assert self.__nombre == otra.get_nombre()
-		assert self.__email == otra.get_email()
-		assert self.__dni == otra.get_dni()
-		assert self.__cuenta_bancaria == otra.get_cuenta_bancaria()
-		assert self.__id_poliza == otra.get_id_poliza()
-		
-		return (self.__nombre == otra.get_nombre() ) and (self.__email == otra.get_email()) and (self.__dni == otra.get_dni()) and (self.__cuenta_bancaria == otra.get_cuenta_bancaria() and (self.__id_poliza == otra.get_id_poliza()))
 
 # Test comparación usuario cliente/asegurado
 def test_compare_usuario_cliente():
-	t1 = TestUsuarioCliente("Carlos", "carlos7ma@gmail.com", "75925767-F", "ES12345678", "12345678")
-	t2 = TestUsuarioCliente("Carlos", "carlos7ma@gmail.com", "75925767-F", "ES12345678", "12345678")
+	t1 = UsuarioCliente("Carlos", "carlos7ma@gmail.com", "75925767-F", "ES12345678", "12345678")
+	t2 = UsuarioCliente("Carlos", "carlos7ma@gmail.com", "75925767-F", "ES12345678", "12345678")
 	assert t1 == t1 # Pasa test
 	assert t1 == t2 # Pasa test
