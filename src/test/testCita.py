592c43f68e6bd827ca79e9c336dcf6016c7b7b13
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-11-11 20:55:33 +0100

    #58 Definición test entidad cita médica

--- src/test/testCita.py
+++ src/test/testCita.py
@@ -1,71 +1,20 @@
-from testUsuarioCliente import TestUsuarioCliente
+import sys
+import os
+sys.path.append(os.path.abspath('./core/'))
+
+
+from usuarioCliente import UsuarioCliente
+from cita import Cita
 from typing import List
 import datetime
 
-# Clase de cita médica
-class TestCita:
-	def __init__(self, id_autorizacion: str, asegurado: TestUsuarioCliente, id_prescripcion: str, fecha: datetime, hora: datetime, facultativo_realizador: str, consulta: str):
-		self.__id_autorizacion = id_autorizacion
-		self.__asegurado = asegurado
-		self.__id_prescripcion = id_prescripcion
-		self.__fecha = fecha
-		self.__hora = hora
-		self.__facultativo_realizador = facultativo_realizador
-		self.__consulta = consulta
-
-	# Métodos get/set
-	def get_id_autorizacion(self):
-		return self.__id_autorizacion
-
-	def get_asegurado(self):
-		return self.__asegurado
-
-	def get_id_prescripcion(self):
-		return self.__id_prescripcion
-
-	def get_fecha(self):
-		return self.__fecha
-
-	def set_fecha(self, fecha):
-		self.__fecha = fecha
-
-	def get_hora(self):
-		return self.__hora
-
-	def set_hora(self, hora):
-		self.__hora = hora
-
-	def get_facultativo_realizador(self):
-		return self.__facultativo_realizador
-
-	def set_facultativo_realizador(self, facultativo_realizador):
-		self.facultativo_realizador = facultativo_realizador
-
-	def get_consulta(self):
-		return self.__consulta
-
-	def set_consulta(self, consulta):
-		self.__consulta = consulta
-
-	# Override método equal
-	def __eq__(self, otra):
-		assert self.__id_autorizacion == otra.get_id_autorizacion()
-		assert self.__asegurado == otra.get_asegurado()
-		assert self.__id_prescripcion == otra.get_id_prescripcion()
-		assert self.__fecha == otra.get_fecha()
-		assert self.__hora == otra.get_hora()
-		assert self.__facultativo_realizador == otra.get_facultativo_realizador()
-		assert self.__consulta == otra.get_consulta()
-
-		return ((self.__id_autorizacion == otra.get_id_autorizacion()) and (self.__asegurado == otra.get_asegurado()) and (self.__id_prescripcion == otra.get_id_prescripcion()) and (self.__fecha == otra.get_fecha()) and (self.__hora == otra.get_hora()) and (self.__facultativo_realizador == otra.get_facultativo_realizador()) and (self.__consulta == otra.get_consulta()))
-
 # Test comparación de cita médica
 def test_compare_cita():
-	u = TestUsuarioCliente("Carlos", "carlos7ma@gmail.com", "75925767-F", "ES12345678", "12345678")
+	u = UsuarioCliente("Carlos", "carlos7ma@gmail.com", "75925767-F", "ES12345678", "12345678")
 	fecha = datetime.datetime(2020, 5, 17)
 	hora = datetime.time(3, 45, 12)
 	
-	t1 = TestCita("AU-77925767-1", u, "PR-77925767-1", fecha, hora, "D. Fernando", "Centro médico capital, Sala 2")
-	t2 = TestCita("AU-77925767-1", u, "PR-77925767-1", fecha, hora, "D. Fernando", "Centro médico capital, Sala 2")
+	t1 = Cita("AU-77925767-1", u, "PR-77925767-1", fecha, hora, "D. Fernando", "Centro médico capital, Sala 2")
+	t2 = Cita("AU-77925767-1", u, "PR-77925767-1", fecha, hora, "D. Fernando", "Centro médico capital, Sala 2")
 	assert t1 == t1 # Pasa test
 	assert t1 == t2 # Pasa test
