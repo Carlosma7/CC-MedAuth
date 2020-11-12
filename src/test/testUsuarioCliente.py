ae93f2227b136244df8c9621862f2da80137643f
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-11-12 02:02:56 +0100

    #68 Eliminado id póliza de usuario cliente en test usuario cliente

--- src/test/testUsuarioCliente.py
+++ src/test/testUsuarioCliente.py
@@ -9,7 +9,7 @@ import pytest
 
 # Test comparación usuario cliente/asegurado
 def test_compare_usuario_cliente():
-	t1 = UsuarioCliente("Carlos", "carlos7ma@gmail.com", "75925767-F", "ES12345678", "12345678")
-	t2 = UsuarioCliente("Carlos", "carlos7ma@gmail.com", "75925767-F", "ES12345678", "12345678")
+	t1 = UsuarioCliente("Carlos", "carlos7ma@gmail.com", "75925767-F", "ES12345678")
+	t2 = UsuarioCliente("Carlos", "carlos7ma@gmail.com", "75925767-F", "ES12345678")
 	assert t1 == t1 # Pasa test
 	assert t1 == t2 # Pasa test
