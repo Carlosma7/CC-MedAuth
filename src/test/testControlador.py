96194cdbd1eb87928999636940dc1ea55167918b
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-11-12 01:48:10 +0100

    #34 #58 Definición test consultar póliza de asegurado

--- src/test/testControlador.py
+++ src/test/testControlador.py
@@ -85,5 +85,14 @@ def test_modificar_poliza():
 			assert_that(polizaAntigua).is_not_equal_to(polizaNueva[0])
 
 
-
+# Test de consulta de póliza
+def test_consultar_poliza():
+	controlador = Controller()
+	cliente = [c for c in controlador.usuariosClientes if c.get_dni() == "75125767-F"]
+	
+	if len(cliente) > 0:
+		fecha = datetime.datetime(2020, 5, 17)
+		poliza1 = Poliza(cliente[0], "MA-75125767-1", fecha, TipoPoliza.Basica, 10.99, 50.99, ["TAC", "Apendicitis"], [ModuloExtra.Dental], True)
+		poliza2 = controlador.consultar_poliza("75125767-F")
+		assert_that(poliza1).is_equal_to(poliza2)
 
