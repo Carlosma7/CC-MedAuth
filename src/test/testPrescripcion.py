a8498788467f2025c8cb508dcb32c688098c6831
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-11-12 02:03:41 +0100

    #68 Eliminado id póliza de usuario cliente en test prescripción

--- src/test/testPrescripcion.py
+++ src/test/testPrescripcion.py
@@ -14,7 +14,7 @@ import datetime
 
 # Test comparación de prescripciones
 def test_compare_prescripcion():
-	u = UsuarioCliente("Carlos", "carlos7ma@gmail.com", "75925767-F", "ES12345678", "12345678")
+	u = UsuarioCliente("Carlos", "carlos7ma@gmail.com", "75925767-F", "ES12345678")
 	fecha = datetime.datetime(2020, 5, 17)
 	
 	t1 = Prescripcion("PR-12345678", u, "MA-75925767-1", fecha, Especialidad.Traumatologia, "D. Fernando", "D. Juan", ["Radiografía", "Ortopedia"], "Centro médico capital, Sala 2")
