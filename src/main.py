0cd4dc54d6e72f63ca3664e1a27e1cd6f2d75d8d
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-11-12 02:10:51 +0100

    #44 Se añade crear usuario cliente

--- src/main.py
+++ src/main.py
@@ -11,3 +11,7 @@ if __name__ == '__main__':
 	# [HU1] Creación usuario administrativo
 	admin = UsuarioAdmin("Carlos", "carlos7ma@gmail.com", "75925767-F", "")
 	controlador.crear_admin(admin)
+	
+	# [HU2] Creación usuario asegurado
+	cliente = UsuarioCliente("Juan", "juan@gmail.com", "7512354-F", "ES12341118")
+	controlador.crear_cliente(cliente)
