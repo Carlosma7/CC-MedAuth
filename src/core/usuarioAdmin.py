d3ad1a56d5b5776757dab66a102ee59fc7a28663
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-12-08 18:48:47 +0100

    #43 Definición Admin con clase padre Usuario

--- src/core/usuarioAdmin.py
+++ src/core/usuarioAdmin.py
@@ -1,27 +1,12 @@
+from usuario import Usuario
+
 # Clase de usuario administrativo
-class UsuarioAdmin():
+class UsuarioAdmin(Usuario):
 	def __init__(self, nombre: str, email: str, dni: str, email_empresarial: str):
-		self.__nombre = nombre
-		self.__email = email
-		self.__dni = dni
 		self.__email_empresarial = email_empresarial
+		super().__init__(nombre, email, dni)
 	
 	# Métodos get/set
-	def get_nombre(self):
-		return self.__nombre
-	
-	def set_nombre(self, nombre: str):
-		self.__nombre = nombre
-		
-	def get_email(self):
-		return self.__email
-		
-	def set_email(self, email: str):
-		self.__email = email
-	
-	def get_dni(self):
-		return self.__dni
-		
 	def get_email_empresarial(self):
 		return self.__email_empresarial
 	
@@ -30,4 +15,4 @@ class UsuarioAdmin():
 	
 	# Override método equal
 	def __eq__(self, otra):
-		return (self.__nombre == otra.get_nombre() ) and (self.__email == otra.get_email()) and (self.__dni == otra.get_dni()) and (self.__email_empresarial == otra.get_email_empresarial())
+		return super().__eq__(otra) and (self.__email_empresarial == otra.get_email_empresarial())
