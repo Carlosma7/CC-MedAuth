193493aaf2555b85a40ad8b5bec7ad390f00a818
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-12-10 13:21:08 +0100

    #58 Definición test significativos pólizas

--- src/test/testPoliza.py
+++ src/test/testPoliza.py
@@ -6,20 +6,21 @@ from usuarioCliente import UsuarioCliente
 from poliza import Poliza
 from tipoPoliza import TipoPoliza
 from moduloExtra import ModuloExtra
-
-
 from typing import List
 import datetime
 
-
 # Test comparación pólizas
 def test_compare_poliza():
+	# Creación usuario cliente
 	u = UsuarioCliente("Carlos", "carlos7ma@gmail.com", "75925767-F", "ES12345678")
-	u2 = UsuarioCliente("Carlos", "carlos7ma@gmail.com", "75925767-F", "ES12345678")
+	# Creación de fecha
 	fecha = datetime.datetime(2020, 5, 17)
-	fecha2 = datetime.datetime(2020, 5, 18)
-	
+	# Creación de dos pólizas idénticas
 	t1 = Poliza(u, "12345678", fecha, TipoPoliza.Basica, 35.99, 103.0, ["TAC", "Apendicitis"], [ModuloExtra.Dental], True)
-	t2 = Poliza(u2, "12345678", fecha, TipoPoliza.Basica, 35.99, 103.0, ["TAC", "Apendicitis"], [ModuloExtra.Dental], True)
-	assert t1 == t1 # Pasa test
+	t2 = Poliza(u, "12345678", fecha, TipoPoliza.Basica, 35.99, 103.0, ["TAC", "Apendicitis"], [ModuloExtra.Dental], True)
+	# Creación de una póliza distinta
+	t3 = Poliza(u, "12345678", fecha, TipoPoliza.Basica, 35.99, 20.0, ["TAC", "Apendicitis"], [ModuloExtra.Dental], True)
+	# Comprobar que una póliza es igual a otra si tienen la misma información
 	assert t1 == t2 # Pasa test
+	# Comprobar que una póliza es distinta de otra si tienen alguna información distinta
+	assert t1 != t3
