e997dbc97a440aa7d1b8d5b23438611990a77ed9
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-12-10 13:26:32 +0100

    #58 Definición test significativos usuarios clientes

--- src/test/testUsuarioCliente.py
+++ src/test/testUsuarioCliente.py
@@ -3,13 +3,16 @@ import os
 sys.path.append(os.path.abspath('./src/core/'))
 
 from usuarioCliente import UsuarioCliente
-
 import pytest
-
 
 # Test comparación usuario cliente/asegurado
 def test_compare_usuario_cliente():
+	# Creación de dos usuarios clientes idénticos
 	t1 = UsuarioCliente("Carlos", "carlos7ma@gmail.com", "75925767-F", "ES12345678")
 	t2 = UsuarioCliente("Carlos", "carlos7ma@gmail.com", "75925767-F", "ES12345678")
-	assert t1 == t1 # Pasa test
+	# Creación de un usuario cliente distinto
+	t3 = UsuarioCliente("Carlos", "carlos7ma@gmail.com", "22225767-F", "ES12345678")
+	# Comprobar que una prescripción es igual a otra si tienen la misma información
 	assert t1 == t2 # Pasa test
+	# Comprobar que una prescripción es distinta de otra si tienen alguna información distinta
+	assert t1 != t3
