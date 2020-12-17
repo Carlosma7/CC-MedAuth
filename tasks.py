e3a791a79c8dc64e849eb5271f9db0bafadd1e6a
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-12-17 18:57:44 +0100

    Close #83 DRY tasks.py resumir código repetido

--- tasks.py
+++ tasks.py
@@ -4,37 +4,13 @@ from invoke import task, run
 @task 
 def clean(c):
 	print("Borrando caché de python.")
-	run("find . -maxdepth 5 -type d -name __pycache__ -exec rm -r {} +")
-	print("Borrando caché de pytest.")
-	run("find . -maxdepth 5 -type d -name .pytest_cache -exec rm -r {} +")
+	run("find . -maxdepth 5 -type d -name __pycache__ -name  .pytest_cache  -exec rm -r {} +")
 
 # Tarea de ejecución de tests
 @task
 def test(c):
 	print("Ejecución de test.\n")
-	print("Test Clase Usuario:")
-	run("pytest -v --disable-pytest-warnings ./src/test/testUsuario.py")
-	
-	print("Test Clase UsuarioAdmin:")
-	run("pytest -v --disable-pytest-warnings ./src/test/testUsuarioAdmin.py")
-	
-	print("\nTest Clase UsuarioCliente:")
-	run("pytest -v --disable-pytest-warnings ./src/test/testUsuarioCliente.py")
-	
-	print("\nTest Clase Poliza:")
-	run("pytest -v --disable-pytest-warnings ./src/test/testPoliza.py")
-	
-	print("\nTest Clase Prescripcion:")
-	run("pytest -v --disable-pytest-warnings ./src/test/testPrescripcion.py")
-	
-	print("\nTest Clase Autorizacion:")
-	run("pytest -v --disable-pytest-warnings ./src/test/testAutorizacion.py")
-	
-	print("\nTest Clase Cita:")
-	run("pytest -v --disable-pytest-warnings ./src/test/testCita.py")
-	
-	print("\nTest Clase Controlador:")
-	run("pytest -v --disable-pytest-warnings ./src/test/testControlador.py")
+	run("pytest -v --disable-pytest-warnings src/test/*")
 
 # Tarea de ejecución del modelo
 @task
