d1b41c8bac4079ff21bff8d9dc83a5efd8a9771d
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-12-17 19:26:28 +0100

    #84 Cambio a assertpy testAutorizacion

--- src/test/testAutorizacion.py
+++ src/test/testAutorizacion.py
@@ -7,6 +7,7 @@ from autorizacion import Autorizacion
 from especialidad import Especialidad
 from typing import List
 import datetime
+from assertpy import assert_that
 
 # Test comparación de autorizaciones	
 def test_compare_autorizacion():
@@ -21,6 +22,6 @@ def test_compare_autorizacion():
 	# Creación de una autorización distinta
 	t3 = Autorizacion("AU-12345678", u, "MA-75925767-1", "PR-12345678", True, "", fecha, Especialidad.Traumatologia, ["Radiografía", "Ortopedia"], "D. Carlos", "Centro médico capital, Sala 2")
 	# Comprobar que una autorización es igual a otra si tienen la misma información
-	assert t1 == t2 # Pasa test
+	assert_that(t1).is_equal_to(t2) # Pasa test
 	# Comprobar que una autorización es distinta de otra si tienen alguna información distinta
-	assert t1 != t3
+	assert_that(t1).is_not_equal_to(t3) # Pasa test
