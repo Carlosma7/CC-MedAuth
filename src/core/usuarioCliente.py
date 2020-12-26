74cd2e8729be349f09a5005c0dc3c9a81122e368
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-12-26 17:36:17 +0100

    Se añade método to_dict()

--- src/core/usuarioCliente.py
+++ src/core/usuarioCliente.py
@@ -16,3 +16,7 @@ class UsuarioCliente(Usuario):
 	# Override método equal
 	def __eq__(self, otra):
 		return super().__eq__(otra) and (self.__cuenta_bancaria == otra.get_cuenta_bancaria())
+		
+	# Método para transformar objeto en un dict
+	def to_dict(self):
+		return {'nombre': super().get_nombre(), 'email': super().get_email(), 'dni': super().get_dni(), 'cuenta_bancaria': self.__cuenta_bancaria}
