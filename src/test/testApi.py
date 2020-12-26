13fecf5fe6025e104d661f9d090582b6ac7293a5
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-12-26 23:43:41 +0100

    #58 Se añaden test api modificar poliza

--- src/test/testApi.py
+++ src/test/testApi.py
@@ -138,3 +138,34 @@ async def test_crear_poliza_api(test_medauth):
 	response = await client.post(url, data = json.dumps(poliza.to_dict()))
 	# Comprobar que el estado es correcto
 	assert_that(response.status_code).is_equal_to(200)
+
+# Test de modificar póliza
+@pytest.mark.asyncio
+async def test_modificar_poliza_api(test_medauth):
+	# Obtener el servidor de la app
+	client = app.test_client()
+	# Crear url
+	url = '/poliza/modificar'
+
+	# Crear usuario administrativo
+	usuario = UsuarioCliente('Roberto', 'rober@gmail.com', '25123540-F', 'ES1234111892738495273840')
+	# Creación fecha
+	fecha = datetime.datetime(2020, 5, 17)
+	# Creación objeto Póliza
+	poliza = Poliza(usuario, "MA-25123540-1", fecha, TipoPoliza.Basica, 5.99, 50.99, ["TAC", "Apendicitis"], [ModuloExtra.Dental], True)
+	# Creación periodo carencia
+	periodo_carencia = fecha.strftime('%m/%d/%Y')
+	# Creación tipo póliza
+	tipo = TipoPoliza.Basica
+	# Creación copagos y mensualidad, cambiando los copagos como modificación
+	copagos = 10.99
+	mensualidad = 50.99
+	# Creación servicios_excluidos
+	servicios_excluidos = ["TAC", "Apendicitis"]
+	# Creación módulos extra
+	modulos_extra = [ModuloExtra.Dental]
+	
+	# Lanzar petición
+	response = await client.post(url, data = json.dumps({'poliza': poliza.to_dict(), 'periodo_carencia': periodo_carencia, 'tipo': tipo, 'copagos': copagos, 'mensualidad': mensualidad, 'servicios_excluidos': servicios_excluidos, 'modulos_extra': modulos_extra}))
+	# Comprobar que el estado es correcto
+	assert_that(response.status_code).is_equal_to(200)
