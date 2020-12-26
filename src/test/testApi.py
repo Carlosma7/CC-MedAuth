ab556b7dce01829449c2825eb6a51fd6ad3ee74a
Carlos-Morales <carlos7ma@correo.ugr.es>
2020-12-26 17:55:38 +0100

    #58 Se añaden test api modificar usuario administrativo

--- src/test/testApi.py
+++ src/test/testApi.py
@@ -44,3 +44,23 @@ async def test_crear_cliente_api(test_medauth):
 	response = await client.post(url, data = json.dumps({'usuario': usuario.to_dict(), 'tipo': tipo}))
 	# Comprobar que el estado es correcto
 	assert_that(response.status_code).is_equal_to(200)
+
+# Test de modificación de administrador
+@pytest.mark.asyncio
+async def test_modificar_admin_api(test_medauth):
+	# Obtener el servidor de la app
+	client = app.test_client()
+	# Crear url
+	url = '/usuario/modificar'
+	
+	# Crear usuario administrativo
+	usuario = UsuarioAdmin('Carlos', 'carlos7ma@gmail.com', '75925767-F', '')
+	tipo = 0
+	nombre = 'Carlos'
+	email = 'charles@gmail.com'
+	cuenta_bancaria = ''
+	
+	# Lanzar petición
+	response = await client.post(url, data = json.dumps({'usuario': usuario.to_dict(), 'tipo': tipo, 'nombre': nombre, 'email': email, 'cuenta_bancaria': cuenta_bancaria}))
+	# Comprobar que el estado es correcto
+	assert_that(response.status_code).is_equal_to(200)
